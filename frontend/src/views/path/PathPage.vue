e7d1597864e3d59a198f9bdf14544abe42c4bbd2
yejiahn <yeppilisa@naver.com>
2020-08-06 22:09:26 +0900

    feat: 현재 시간을 보여주는 getCurrentTime 메서드 구현

--- frontend/src/views/path/PathPage.vue
+++ frontend/src/views/path/PathPage.vue
@@ -207,6 +207,14 @@ export default {
   computed: {
     ...mapGetters(['stations', 'pathResult']),
     getCurrentTime() {
+      const d = new Date();
+      const hours = d.getHours();
+      const dayTime = hours>12? "오후":"오전";
+        this.departureTimeView = {
+          dayTime: dayTime,
+          hour: hours,
+          minute: d.getMinutes()
+      };
       const { hour, minute } = this.departureTimeView
       return `${hour > 12 ? '오후' : '오전'} ${hour < 10 ? `0${hour}` : hour}:${minute < 10 ? `0${minute}` : minute}`
     }
